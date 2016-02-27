b276a7e69240275d92ffccc010ba0c8099f2a65a
Tom Elliff <tomelliff@gmail.com>
2016-02-27 23:33:01 +0000

    Add basic error handling

--- hello_world.py
+++ hello_world.py
@@ -4,4 +4,10 @@ import json
 
 def lambda_handler(event, context):
   print("Received event: {0}".format(json.dumps(event, indent=2)))
-  return "Hello {0}!".format(event['object'])
+  try:
+    event_object = event['object']
+  except KeyError:
+    raise KeyError("Event should contain a key called 'object'")
+  if event_object != "world":
+    raise ValueError("This is a 'Hello world!' function, not 'Hello {0}!".format(event_object))
+  return "Hello {0}!".format(event_object)
